32b72916d4c3b2704c2f72c3362aac680b638b78
argorothtechnolgy-admin <robotmaster1989@outlook.com>
2023-08-22 07:35:03 +0000

    ok

--- config/app.php
+++ config/app.php
@@ -57,7 +57,7 @@ return [
 
     'url' => env('APP_URL', 'http://localhost'),
 
-    'asset_url' => env('ASSET_URL','https://www.argorothtechnolgy.net'),
+    'asset_url' => env('ASSET_URL'),
 
     /*
     |--------------------------------------------------------------------------
